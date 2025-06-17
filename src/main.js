40872f3b3f2b715c3fc2d84eb4b1195dcb8464f1
Eric Phifer <ericoncode@gmail.com>
2025-06-17 03:40:51 -0600

    set up sentry for debugging

--- src/main.js
+++ src/main.js
@@ -4,7 +4,18 @@ import App from './App.vue'
 import { router } from './router'
 import { store } from './store'
 
+import * as Sentry from '@sentry/vue'
+
 const app = createApp(App)
+
+Sentry.init({
+  app,
+  dsn: "https://e9d392fb3a36ebaaa0fa126b54c890fe@o4509513318400000.ingest.us.sentry.io/4509513320955904",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true
+});
+
 app.use(store)
 app.use(router)
 app.mount('#app')
