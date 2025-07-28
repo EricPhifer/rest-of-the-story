1fa831505e1e9d800791262b0467bd44c47aea97
Eric Phifer <ericoncode@gmail.com>
2025-07-28 09:39:48 -0600

    adjust height of hero section & complete phase 1 navigation

--- studio/schemaTypes/navSchemas/navigation.js
+++ studio/schemaTypes/navSchemas/navigation.js
@@ -1,26 +1,61 @@
-import { defineType } from 'sanity'
+import { defineType, defineField } from 'sanity'
 
 export default defineType({
   name: 'navigation',
   title: 'Navigation',
   type: 'document',
   fields: [
-    {
+    defineField({
       name: 'logo',
       title: 'Logo',
       type: 'image',
-      options: {
-        hotspot: true
-      },
+      options: { hotspot: true },
       description: 'Upload your site logo (used in the header).'
-    },
-    {
+    }),
+    defineField({
       name: 'grandOpeningMessage',
-      title: 'Grand Opening Message',
+      title: 'Message',
       type: 'array',
       of: [{ type: 'block' }],
-      description: 'Text shown next to the logo to announce the grand opening.'
-    }
+      description: 'Text shown next to the logo.'
+    }),
+    defineField({
+      name: 'links',
+      title: 'Navigation Links',
+      type: 'array',
+      of: [
+        {
+          type: 'object',
+          title: 'Link',
+          fields: [
+            {
+              name: 'label',
+              title: 'Label',
+              type: 'string',
+              validation: Rule => Rule.required()
+            },
+            {
+              name: 'internalLink',
+              title: 'Internal Page',
+              type: 'reference',
+              to: [
+                { type: 'page' },
+                { type: 'faqPage' },
+                { type: 'sitemapPage' },
+                { type: 'legalPage' },
+              ]
+            }
+          ],
+          preview: {
+            select: {
+              title: 'label',
+              subtitle: 'internalLink.slug.current'
+            }
+          }
+        }
+      ],
+      description: 'Set the main navigation links in the header.'
+    })
   ],
   preview: {
     select: {
