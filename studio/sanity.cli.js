117a21aeb220b21c84f01e879ef01e37c5c47d9b
EricPhifer <ericoncode@gmail.com>
2026-02-02 06:15:58 -0700

    update santiy cli

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -6,12 +6,12 @@ export default defineCliConfig({
     projectId: 'xbjigamf',
     dataset: 'production'
   },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
-  autoUpdates: true,
   deployment: {
     appId: 'v4quqqqd7zus7kqi18rys1xl',
+    /**
+     * Enable auto-updates for studios.
+     * Learn more at https://www.sanity.io/docs/cli#auto-updates
+     */
+    autoUpdates: true,
   }
 })
